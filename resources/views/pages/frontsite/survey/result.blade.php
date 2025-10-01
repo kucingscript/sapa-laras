aaed74f83c8e590da0194c5f1ea5a1951e2119cc
BokutoKotarou <rasyidzkun@gmail.com>
2025-10-01 14:36:38 +0700

    fix: result card

--- resources/views/pages/frontsite/survey/result.blade.php
+++ resources/views/pages/frontsite/survey/result.blade.php
@@ -100,3 +100,20 @@
         </div>
     </div>
 @endsection
+
+@push('script')
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+            const header = document.getElementById('main-header');
+            const scrollThreshold = 50;
+
+            window.addEventListener('scroll', () => {
+                if (window.scrollY > scrollThreshold) {
+                    header.classList.add('bg-white', 'shadow-lg');
+                } else {
+                    header.classList.remove('bg-white', 'shadow-lg');
+                }
+            });
+        })
+    </script>
+@endpush
